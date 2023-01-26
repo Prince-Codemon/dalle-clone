a01084f0188a37fdd1559675daacb0ab22ab5a1d
PRINCE <94777825+Prince-Codemon@users.noreply.github.com>
2023-01-26 19:43:18 +0530

    a;

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
-import connectDB from './mongodb/connect.js'
-import postRoutes from './routes/postRoutes.js'
-import dalleRoutes from './routes/dalleRoutes.js'
-import cors from 'cors'
+// import connectDB from './mongodb/connect.js'
+// import postRoutes from './routes/postRoutes.js'
+// import dalleRoutes from './routes/dalleRoutes.js'
+// import cors from 'cors'
 const app = express()
 // connectDB()
 const PORT = process.env.PORT || 3000 
@@ -19,8 +19,8 @@ const auth = (req,res,next)=>{
 
 app.use(cors()) 
 app.use(express.json({limit: '50mb'}))
-app.use('/api/v1/posts',auth,postRoutes)
-app.use('/api/v1/dalle',auth,dalleRoutes)
+// app.use('/api/v1/posts',auth,postRoutes)
+// app.use('/api/v1/dalle',auth,dalleRoutes)
  
 
 app.get('/',(req,res)=>{
