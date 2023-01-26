c6579189022a2b3fa34ae1206525a3f04322f0bd
PRINCE <94777825+Prince-Codemon@users.noreply.github.com>
2023-01-26 19:59:58 +0530

    a;

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const connectDB = require('./config/db')
+const connectDB = require('./mongodb/connect')
 
 const app = express()
 connectDB()
