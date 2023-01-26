6195fd1201ec31ce373117d69b662d4fd7d87684
PRINCE <94777825+Prince-Codemon@users.noreply.github.com>
2023-01-26 23:44:46 +0530

    f

--- server/index.js
+++ server/index.js
@@ -25,24 +25,24 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use("/api/v1/posts", auth, postRoutes);
 // app.('/api/v1/dalle',auth,dalleRoutes)
-app.post("/api/v1/dalle", auth, async (req, res) => {
-  try {
-    const { prompt } = req.body;
-    // const aiResponse = await openai.createImage({
-    //   prompt,
-    //   n: 1,
-    //   size: "1024x1024",
-    //   response_format: "b64_json",
-    // });
-    // const image = aiResponse.data.data[0].b64_json;
-    res.status(200).json({ photo: prompt });
-  } catch (error) {
-    console.error(error);
-    res
-      .status(400)
-      .send(error?.statusText || "Something went wrong");
-  }
-});
+// app.post("/api/v1/dalle", auth, async (req, res) => {
+//   try {
+//     const { prompt } = req.body;
+//     // const aiResponse = await openai.createImage({
+//     //   prompt,
+//     //   n: 1,
+//     //   size: "1024x1024",
+//     //   response_format: "b64_json",
+//     // });
+//     // const image = aiResponse.data.data[0].b64_json;
+//     res.status(200).json({ photo: prompt });
+//   } catch (error) {
+//     console.error(error);
+//     res
+//       .status(400)
+//       .send(error?.statusText || "Something went wrong");
+//   }
+// });
 
 app.get("/", (req, res) => {
   res.send("Hello World");
