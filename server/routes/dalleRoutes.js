6a12aa70ce0b72fa976e31262c56127bda324324
PRINCE <94777825+Prince-Codemon@users.noreply.github.com>
2023-01-26 23:25:31 +0530

    f

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -7,45 +7,22 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 router.post("/", async (req, res) => {
-  // try {
-  //   const { prompt } = req.body;
-  //   if(!prompt) return res.status(400).json({message: "Prompt is required"} )
-  //   const response = await openai.createImage({
-  //     prompt: prompt,
-  //     n: 1,
-  //     size: "512x512",
-  //     response_format: "b64_json",
-  //   });
-  //   const image =  response?.data?.data[0].b64_json;
-  //   return res.status(200).json({ photo: image });
-  // } catch (error) {
-  //   return res
-  //     .status(500)
-  //     .send(error?.response.data.error.message);
-  // }
-
-  router.post('/', async (req, res) => {
-    try {
-      const { prompt } = req.body;
-      const aiResponse = await openai.createImage({
-        prompt,
-        n: 1,
-        size: "1024x1024",
-        response_format: "b64_json",
-      });
-      const image = aiResponse.data.data[0].b64_json;
-      res.status(200).json({ photo: image });
-    } catch (error) {
-      console.error(error);
-      res
-        .status(400)
-        .send(error?.response.data.error.message || "Something went wrong");
-    }
-  });
-    
+  try {
+    const { prompt } = req.body;
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+    const image = aiResponse.data.data[0].b64_json;
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(400)
+      .send(error?.response.data.error.message || "Something went wrong");
+  }
 });
-
-
-
 
 module.exports = router;
