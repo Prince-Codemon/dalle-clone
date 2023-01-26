d637686141c6b39043789927fec47bee1d436878
PRINCE <94777825+Prince-Codemon@users.noreply.github.com>
2023-01-26 20:28:35 +0530

    a;

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -7,22 +7,22 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-// router.post("/", async (req, res) => {
-//   try {
-//     const { prompt } = req.body;
-//     const response = await openai.createImage({
-//       prompt: prompt,
-//       n: 1,
-//       size: "512x512",
-//       response_format: "b64_json",
-//     });
-//     const image =  response.data.data[0].b64_json;
-//     return res.status(200).json({ photo: image });
-//   } catch (error) {
-//     return res
-//       .status(500)
-//       .send(error?.response.data.error.message || error.message);
-//   }
-// });
+router.post("/", async (req, res) => {
+  try {
+    const { prompt } = req.body;
+    const response = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "512x512",
+      response_format: "b64_json",
+    });
+    const image =  response.data.data[0].b64_json;
+    return res.status(200).json({ photo: image });
+  } catch (error) {
+    return res
+      .status(500)
+      .send(error?.response.data.error.message || error.message);
+  }
+});
 
 module.exports = router;
