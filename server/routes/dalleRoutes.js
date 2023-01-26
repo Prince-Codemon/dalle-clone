89cc9b00fa4900e86060dd94d9f2786ff73ab702
PRINCE <94777825+Prince-Codemon@users.noreply.github.com>
2023-01-26 20:23:45 +0530

    a;

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -2,27 +2,27 @@ const express = require("express");
 require("dotenv").config();
 const { OpenAIApi, Configuration } = require("openai");
 const router = express.Router();
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
 
-router.post("/", async (req, res) => {
-  try {
-    const { prompt } = req.body;
-    const response = await openai.createImage({
-      prompt: prompt,
-      n: 1,
-      size: "512x512",
-      response_format: "b64_json",
-    });
-    const image = response.data.data[0].b64_json;
-    return res.status(200).json({ photo: image });
-  } catch (error) {
-    return res
-      .status(500)
-      .send(error?.response.data.error.message || error.message);
-  }
-});
+// router.post("/", async (req, res) => {
+//   try {
+//     const { prompt } = req.body;
+//     const response = await openai.createImage({
+//       prompt: prompt,
+//       n: 1,
+//       size: "512x512",
+//       response_format: "b64_json",
+//     });
+//     const image =  response.data.data[0].b64_json;
+//     return res.status(200).json({ photo: image });
+//   } catch (error) {
+//     return res
+//       .status(500)
+//       .send(error?.response.data.error.message || error.message);
+//   }
+// });
 
 module.exports = router;
