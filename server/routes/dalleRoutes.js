33d2793d081634f17a8a1769297c2085030b9312
PRINCE <94777825+Prince-Codemon@users.noreply.github.com>
2023-01-26 20:15:57 +0530

    a;

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,15 +1,13 @@
 const express = require("express");
 require("dotenv").config();
 const { OpenAIApi, Configuration } = require("openai");
-
 const router = express.Router();
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-
-router.route("/").post(async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const { prompt } = req.body;
     const response = await openai.createImage({
@@ -18,9 +16,7 @@ router.route("/").post(async (req, res) => {
       size: "512x512",
       response_format: "b64_json",
     });
-
     const image = response.data.data[0].b64_json;
-
     return res.status(200).json({ photo: image });
   } catch (error) {
     return res
